e7256a0fc2c72cabcab55841bfb1629e25c50a83
LuisFSC1227 <99209493+LuisFSC1227@users.noreply.github.com>
2023-02-27 16:46:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro Projeto</title>
+</head>
+<body>
+    <h1>Ola Mundo</h1>
+<h2> 3+2 =
+    <?php
+    echo(3+2)
+    ?>
+</h2>
+
+<h2> 6+5 =
+    <?php
+    echo(6+5)
+    ?>
+</h2>  
+
+<h2> 10+2 =
+    <?php
+    echo(10+2)
+    ?>
+</h2>  
+
+<h2> 4+5 =
+    <?php
+    echo(4+5)
+    ?>
+</h2>  
+</body>
+</html>
